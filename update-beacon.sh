e5ef11c3944b6f5fc34b856aa407ef554790b320
click2install <click2install@users.noreply.github.com>
2019-02-27 02:50:44 +1100

    Add files via upload

---
+++ update-beacon.sh
@@ -0,0 +1,13 @@
+sudo -i
+systemctl stop beacon-mn1
+rm -f /usr/local/bin/beacon*
+tmp=$(mktemp -d)
+cd ${tmp}
+wget https://github.com/beaconcrypto/beacon/releases/download/v1.1.3.0/ubuntu16-4-daemon.zip
+unzip ubuntu16-4-daemon.zip --strip=1
+chmod +x beacon*
+mv beacon* /usr/local/bin
+cd 
+rm -rf ${tmp}
+systemctl start beacon-mn1
+exit
