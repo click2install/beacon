a149327f43dcc0ec284d361f0b9a04bae6510c71
click2install <click2install@users.noreply.github.com>
2019-02-27 03:03:35 +1100

    Update install-beacon.sh

--- install-beacon.sh
+++ install-beacon.sh
@@ -9,7 +9,7 @@
 TMP_FOLDER=$(mktemp -d)
 
 DAEMON_ARCHIVE=${1:-"https://github.com/beaconcrypto/beacon/releases/download/v1.1.3.0/ubuntu16-4-daemon.zip"}
-ARCHIVE_STRIP=" --strip=1"
+ARCHIVE_STRIP=""
 DEFAULT_PORT=11115
 
 NODE_IP=$(curl icanhazip.com -s)
